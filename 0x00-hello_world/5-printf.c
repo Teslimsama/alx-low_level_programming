ec7a5a5f779646871ccea370f95bd4a738671e61
Teslimsama <bolajiteslim05@gmail.com>
2023-02-16 19:38:47 -0800

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
 /* Return :0 (success)*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-/* main entry */
