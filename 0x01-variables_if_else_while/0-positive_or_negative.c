14f957c710a23e6840ca35c5c0e72a54162e4db1
Teslimsama <bolajiteslim05@gmail.com>
2023-02-17 12:02:48 -0800

    if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 (success)
+*/
+int main(void)
+{>>0-positive_or_negative.c
+echo {>>0-positive_or_negative.c
+echo {
+{
